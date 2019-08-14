81807ab9d19b635aafe08e869db00d7faefce5ed
sonniakemmer <brownbugz@gmail.com>
2019-08-14 14:11:10 -0700

    8-14-2019 211pm

---
+++ models/movie.js
@@ -0,0 +1,18 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var movieSchema = new Schema({
+    title: String,
+    releaseYear: Number,
+    mpaaRating: String,
+    cast: [String],
+    nowShowing: Boolean
+});
+
+
+
+// compile the schema into a model and export it
+module.exports = mongoose.model('Movie', movieSchema);
+
+
